72c85e7c9c9e6e4762ed1d0ea008368deacfa995
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-06-16 23:03:07 +0100

    modified my portfolio

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var myToggle = document.getElementById("home");
+var myToggle = document.getElementById("nav-toggle");
 var myList = document.getElementById("nav-list")
 
-function screenResize(){
-    screenSize = document.body.clientWidth;
-    if(screenSize <= 576){
-        myToggle.style.display = "block";
-        myToggle.innerHTML = "MENU";
-        myList.style.display = "none";
-    } else{
-        myToggle.style.display = "none";
-        myList.style.display = "flex";
-    }
-}
+// function screenResize(){
+//     screenSize = document.body.clientWidth;
+//     if(screenSize <= 576){
+//         myToggle.style.display = "block";
+//         myToggle.innerHTML = "MENU";
+//         myList.style.display = "none";
+//     } else{
+//         myToggle.style.display = "none";
+//         myList.style.display = "flex";
+//     }
+// }
 
-function showNav(){
-    if(myList.style.display == "none"){
-        myToggle.innerHTML = "CLOSE";
-        myList.style.display = "block";
-    } else{
-        myToggle.innerHTML = "MENU";
-        myList.style.display = "none";
-    }
+// function showNav(){
+//     if(myList.style.display == "none"){
+//         myToggle.innerHTML = "CLOSE";
+//         myList.style.display = "block";
+//     } else{
+//         myToggle.innerHTML = "MENU";
+//         myList.style.display = "none";
+//     }
+// }
+
+
+let showMenu = () => {
+    myList.classList.toggle("nav-list-show")
 }
+myToggle.addEventListener("click", showMenu)
